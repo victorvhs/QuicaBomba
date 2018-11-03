d09fe2dde7903216176be0a255e7b6bcae76bc50
Vhreis <victor.h.s.reis@gmail.com>
2018-11-03 17:51:46 -0300

    Pontos

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 export (int)  var vel = 200
 var velocity = Vector2()
 var vida = 3
+var pt = 0
 
 func _ready():
 	#set_physics_process(true)
@@ -19,5 +20,9 @@ func _physics_process(delta):
 	if col:
 		if col.collider.has_method("hit"):
 			col.collider.hit(col.collider)
-	
-	
+			addPoint(1)
+func addPoint(point):
+	pt +=1
+	var txt_no = get_node('../pontos')
+	var txt ="PONTOS: "
+	txt_no.set_text(txt+ str(pt))
--- scripts/util.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-#Todo Atulizar Placar
-#TODO Criar colisão no chão
-
-func point(point):
-	get_tree().get
